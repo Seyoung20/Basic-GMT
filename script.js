8367407b43b52ee06f9201457bd0a180a5b4ee61
Lornz18 <mellgodwinbarza22@gmail.com>
2024-03-18 10:06:01 +0800

    FRONT PAGE DONE 100%

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ burgir.addEventListener("click", () =>{
     burgir.classList.toggle("open");
     header.classList.toggle("open");
 })
-
+// SCROLL WEBSITE
   document.addEventListener('DOMContentLoaded', function () {
     const header = document.querySelector('.header');
   
     window.addEventListener('scroll', function () {
-      if (window.scrollY > 50) {
+      if (window.scrollY > 100) {
         header.classList.add('header-scrolled');
       } else {
         header.classList.remove('header-scrolled');
